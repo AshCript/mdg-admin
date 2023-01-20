7f5dae51253c8fb4bc38cf907e186d77bb773249
AshCript <asjosvah@gmail.com>
2023-01-20 16:22:39 +0300

    Create a function findAllByRegion() for district route

--- src/routes/district.ts
+++ src/routes/district.ts
@@ -3,12 +3,14 @@ import add from './district/add';
 import findAll from './district/findAll';
 import findById from './district/findById';
 import init from './district/init';
+import findAllByRegion from './district/findAllByRegion';
 
 const district = (app: Express) => {
   add(app)
   init(app)
   findAll(app)
   findById(app)
+  findAllByRegion(app)
 }
 
 export default district
---
+++ src/routes/district/findAllByRegion.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { Express } from 'express-serve-static-core';
+import { District, Region } from '../../db/sequelize';
+
+
+const findAllByRegion = (app: Express) => {
+  app.get('/api/districts/r/:regionId', (req: Request, res: Response) => {
+    const regionId = req.params.regionId
+    Region.findByPk(regionId).then((region: any): any => {
+      if(region === null){
+        const message = `The regions's foreign key with ID ${regionId} doesn't exist. Retry with another region ID!`
+        return res.status(404).json({ message })
+      }
+      return District.findAll({where: {
+        regionId
+      }}).then(districts => {
+        const found = `${districts.length} ${districts.length === 1 ? 'district' : 'districts'} found.`
+        const message = `${districts.length === 0 ? found : 'All districts for region ' + region.name + ' have been loaded! ' + found}`
+        res.json({ message, data: districts })
+      })
+    }).catch(e => {
+      const message = "Something went wrong. Retry later!"
+      res.status(500).json({ message, data: e })
+    })
+  })
+}
+
+export default findAllByRegion
