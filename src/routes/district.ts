7741ec95002e30718790e3f0f20969b8bf782763
AshCript <asjosvah@gmail.com>
2023-01-20 15:40:07 +0300

    Create a function findById() for district route

--- src/routes/district.ts
+++ src/routes/district.ts
@@ -1,12 +1,14 @@
 import { Express } from 'express-serve-static-core';
 import add from './district/add';
 import findAll from './district/findAll';
+import findById from './district/findById';
 import init from './district/init';
 
 const district = (app: Express) => {
   add(app)
   init(app)
   findAll(app)
+  findById(app)
 }
 
 export default district
---
+++ src/routes/district/findById.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { Express } from 'express-serve-static-core';
+import { District } from '../../db/sequelize';
+
+
+const findById = (app: Express) => {
+  app.get('/api/district/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+    District.findByPk(id).then(district => {
+      if(district === null){
+        const message = `District with ID ${id} doesn't exist. Try with another ID!`
+        return res.status(404).json({ message })
+      }
+      const message = `A district with ID ${id} has been found!`
+      res.json({ message, data: district })
+    }).catch(e => {
+      const message = "Impossible to load district. Retry later!"
+      res.status(500).json({ message, data: e })
+    })
+  })
+}
+
+export default findById
