b4fb80e23bddb2a9cd2a66656706a52f89579d36
AshCript <asjosvah@gmail.com>
2023-01-20 15:22:01 +0300

    Import & use findAll() function in district.ts route file

--- src/routes/district.ts
+++ src/routes/district.ts
@@ -1,10 +1,12 @@
 import { Express } from 'express-serve-static-core';
 import add from './district/add';
+import findAll from './district/findAll';
 import init from './district/init';
 
 const district = (app: Express) => {
   add(app)
   init(app)
+  findAll(app)
 }
 
 export default district
