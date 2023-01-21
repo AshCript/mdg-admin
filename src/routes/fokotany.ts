da758e6e50c674a1a73e2721ffcfa41e7a1a6ed1
AshCript <asjosvah@gmail.com>
2023-01-21 16:41:01 +0300

    Creating route for fokotany

---
+++ src/routes/fokotany.ts
@@ -0,0 +1,8 @@
+import { Express } from 'express-serve-static-core';
+
+
+const fokotany = (app: Express) => {
+
+}
+
+export default fokotany
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,12 +3,14 @@ import district from "./district"
 import province from "./province"
 import region from "./region"
 import commune from './commune';
+import fokotany from './fokotany';
 
 const routes = (app: Express) => {
   province(app)
   region(app)
   district(app)
   commune(app)
+  fokotany(app)
 }
 
 export default routes
