7b251bfcfb650e52c53d514ceaaf6b3e1f13a935
AshCript <asjosvah@gmail.com>
2023-01-21 13:50:25 +0300

    Remaking remove() function for province to allow deleting all relations depending to it

--- src/routes/province/remove.ts
+++ src/routes/province/remove.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { Express } from 'express-serve-static-core'
-import { Province } from '../../db/sequelize'
+import { Province, Region, District, Commune } from '../../db/sequelize'
 
 const remove = (app: Express) => {
   app.delete('/api/province/:id', (req: Request, res: Response) => {
@@ -10,6 +10,31 @@ const remove = (app: Express) => {
         const message = `Province with id ${id} NOT FOUND!`
         return res.status(404).json({ message })
       }
+
+      // Delete all relations linked to the province ID that we want to delete
+      Region.findAll({
+        where: {provinceId: province.id}
+      }).then(regions => {
+        regions.map(async (region: any) => {
+          const regionDestroy = Region.destroy({where: {id: region.id}})
+          District.findAll({
+            where: {regionId: region.id}
+          }).then(districts => {
+            districts.map(async (district: any) => {
+              const districtDestroy = District.destroy({where: {id: district.id}})
+              Commune.findAll({
+                where: {districtId: district.id}
+              }).then(communes => {
+                communes.map(async (commune: any) => {
+                  const communeDestroy = await Commune.destroy({where: {id: commune.id}})
+                })
+              })
+            })
+          })
+        })
+      })
+
+      // Delete the province with the ID
       return Province.destroy({where: {id: province.id}}).then(_ => {
         const message = `Province with id ${province.id} deleted successfully!`
         res.json({message, data: province})
