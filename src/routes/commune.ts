ace65daf8d9dd0b7db47ef9b5df78c720b00adc0
AshCript <asjosvah@gmail.com>
2023-01-21 13:11:13 +0300

    Create a function findAll() for commune route

--- src/routes/commune.ts
+++ src/routes/commune.ts
@@ -1,8 +1,11 @@
 import { Express } from 'express-serve-static-core';
+import add from './commune/add';
+import findAll from './commune/findAll';
 
 
 const commune = (app: Express) => {
-
+  add(app)
+  findAll(app)
 }
 
 export default commune
---
+++ src/routes/commune/findAll.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { Express } from 'express-serve-static-core';
+import { Commune } from '../../db/sequelize';
+
+
+const findAll = (app: Express) => {
+  app.get('/api/districts', (req: Request, res: Response) => {
+    Commune.findAll({order: [['id', 'ASC']]}).then(communes => {
+      const found = `${communes.length} ${communes.length === 1 ? 'district' : 'communes'} found!`
+      const message = `${communes.length === 0 ? found : "All communes data have been loaded successfully! " + found }`
+      res.json({ message, data: communes })
+    }).catch(e => {
+      const message = "Impossible to load all communes. Retry later!"
+      res.status(500).json({ message, data: e })
+    })
+  })
+}
+
+export default findAll
