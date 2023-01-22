cb13c3c03505d743fb83cc032e6c7170f2582dee
AshCript <asjosvah@gmail.com>
2023-01-22 11:30:05 +0300

    Creating a function findById() for fokotany route.

--- src/routes/fokotany.ts
+++ src/routes/fokotany.ts
@@ -1,9 +1,11 @@
 import { Express } from 'express-serve-static-core';
 import add from './fokotany/add';
+import findById from './fokotany/findById';
 
 
 const fokotany = (app: Express) => {
   add(app)
+  findById(app)
 }
 
 export default fokotany
---
+++ src/routes/fokotany/findById.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { Express } from 'express-serve-static-core';
+import { Fokotany } from '../../db/sequelize';
+
+
+const findById = (app: Express) => {
+  app.get('/api/fokotany/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+    Fokotany.findByPk(id).then((fokotany: any) => {
+      if(fokotany === null){
+        const message = `Fokotany with ID ${id} doesn't exist. Try another ID!`
+        return res.status(404).json({ message })
+      }
+      const message = `A Fokotany has been found!`
+      res.json({ message, data: fokotany })
+    }).catch(e => {
+      const message = "Something went wrong. Retry later!"
+      res.status(500).json({ message, data: e })
+    })
+  })
+}
+
+export default findById
