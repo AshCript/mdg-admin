9a559bc4810e2919992846c37b37a435c5f99e3e
AshCript <asjosvah@gmail.com>
2023-01-22 11:44:20 +0300

    Creating a function update() for fokotany route.

--- src/routes/fokotany.ts
+++ src/routes/fokotany.ts
@@ -2,12 +2,14 @@ import { Express } from 'express-serve-static-core';
 import add from './fokotany/add';
 import findById from './fokotany/findById';
 import findAll from './fokotany/findAll'
+import update from './fokotany/update';
 
 
 const fokotany = (app: Express) => {
   add(app)
   findById(app)
   findAll(app)
+  update(app)
 }
 
 export default fokotany
---
+++ src/routes/fokotany/update.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { Express } from 'express-serve-static-core';
+import { Fokotany } from '../../db/sequelize';
+
+
+const update = (app: Express) => {
+  app.put('/api/fokotany/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+    const newFokotany = req.body
+    Fokotany.findByPk(id).then((fokotany): any => {
+      if(fokotany === null){
+        const message = `Fokotany with ID ${id} doesn't exist. Try with another ID!`
+        return res.status(404).json({ message })
+      }
+      return Fokotany.update(req.body, {where: {id}}).then(_ => {
+        const message = `Fokotany with ID ${id} updated successfully!`
+        res.json({ message, data: {old: fokotany, new: newFokotany}})
+      })
+    }).catch(e => {
+      const message = "Impossible to update fokotany. Retry later!"
+      res.status(500).json({ message })
+    })
+  })
+}
+
+export default update
